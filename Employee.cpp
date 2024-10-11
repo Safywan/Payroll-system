22fa78585512bf2113606002d927866bb2fae334
Soumikdeb-wq <a1915138@adelaide.edu.au>
2024-10-11 13:26:15 +1030

    Setter and getter for hours worked

--- Employee.cpp
+++ Employee.cpp
@@ -2,8 +2,8 @@
 
 // Constructor initialization
 Employee::Employee(const std::string &name, int employeeID, int age, bool isActive,
-                   std::string position, WorkType workType, float payRate)
-    : position(position), workType(workType), payRate(payRate),
+                   std::string position, WorkType workType, float payRate, int hoursWorked)
+    : position(position), workType(workType), payRate(payRate), hoursWorked(hoursWorked),
       User(name, employeeID, age, isActive)
 {
 }
@@ -13,8 +13,18 @@ float Employee::getPayRate() const {
     return payRate;
 }
 
+// Getter for hoursWorked
+int Employee::getHoursWorked() const {
+    return hoursWorked;
+}
+
+// Setter for hoursWorked
+void Employee::setHoursWorked(int hours) {
+    this->hoursWorked = hours;
+}
+
 // Method to calculate total payment
-float Employee::calculateTotalPayment(int hoursWorked) const {
+float Employee::calculateTotalPayment() const {
     if (workType == FullTime) {
         // Full-time employees have a fixed salary (payRate represents salary)
         return payRate;
