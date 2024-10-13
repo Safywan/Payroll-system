f6791e525082c6c85c254547087381ec17ddb6a5
Kingteena <a1930250@student.adelaide.edu.au>
2024-10-13 15:07:31 +1030

    Add more eatures in manger-main.cpp

--- manager-main.cpp
+++ manager-main.cpp
@@ -8,61 +8,25 @@
 
 using namespace std;
 
-int main()
+
+void removeEmployee(Payroll payroll)
 {
-    // Ask how much is in the company account
-    double initCompanyAmount;
-    cout << "Creating new Payroll..." << endl;
-    cout << "How much is in the company account: " << endl;
-    cin >> initCompanyAmount;
-
-    // Initalise Payroll
-    Payroll payroll = Payroll(initCompanyAmount);
-    int response;
-    do 
-    {
-        cout << "What do you want to do? (Select the number of the option you want)" << endl;
-        cout << "1. Exit" << endl;
-        cout << "2. Add a new Employee" << endl;
-        cout << "3. Remove an Employee" << endl;
-
-        cin >> response;
-
-        switch (response)
-        {
-        case 1:
-            cout << "Exiting program...";
-            break;
-        case 2:
-            addNewEmployee(payroll);
-        case 3:
-            removeEmployee(payroll);
-        default:
-            cout << "Invalid number";
-        }
-    } while (response != 1);
-    
-    addNewEmployee(payroll);
-
-
-    return 0;
-}
-
-void removeEmployee(Payroll payroll){
     // Remove Employee from payroll by prompting user for employeeID
-    cout << "Enter the ID of the Employee you are trying to remove: ";
     int id;
+    cout << "Enter the ID of the Employee you are trying to remove: ";
     cin >> id;
     payroll.removeEmployee(id);
 }
 
-void addNewEmployee(Payroll payroll){
+void addNewEmployee(Payroll payroll)
+{
     // Create new Employee by prompting user for the details and add it to the payroll
     std::string name, position;
     int employeeID, age, worktypeint;
     WorkType worktype;
     double payrate;
 
+    // prompting the user for all the details
     cout << "Enter the Name: ";
     cin >> name;
     cout << "Enter the EmployeeID: ";
@@ -95,7 +59,62 @@ void addNewEmployee(Payroll payroll){
         break;
     }
 
+    // Add new employee to payroll
     Employee newEmployee = Employee(name, employeeID, age, true, position, worktype, payrate);
-    
     payroll.addEmployee(newEmployee);
+}
+
+int main()
+{
+    // Ask how much is in the company account
+    double initCompanyAmount;
+    cout << "Creating new Payroll..." << endl;
+    cout << "How much is in the company account: " << endl;
+    cin >> initCompanyAmount;
+
+    // Initalise Payroll
+    Payroll payroll = Payroll(initCompanyAmount);
+    int response;
+
+    do // Iterate over all the options until the user exits the program
+    {
+        // Print options and ask user for response
+        cout << "What do you want to do? (Select the number of the option you want)" << endl;
+        cout << "1. Exit" << endl;
+        cout << "2. Add a new Employee" << endl;
+        cout << "3. Remove an Employee" << endl;
+        cout << "4. Output total company cost" << endl;
+        cout << "5. Generate Payslips for all Employees" << endl;
+        cout << "6. View Employee details" << endl;
+        cout << "Enter your response: " << endl;
+
+        cin >> response;
+
+        switch (response)
+        {
+        case 1: // Exit
+            cout << "Exiting program...";
+            break;
+        case 2: // Add a New Employee
+            addNewEmployee(payroll);
+            break;
+        case 3: // Remove an Employee
+            removeEmployee(payroll);
+            break;
+        case 4: // Output total company cost
+            cout << "The total cost is: " << payroll.calculateTotalCost() << endl;
+            break;
+        case 5: // Generate payslips
+            cout << "Generating Payslips for all Employees." << endl;
+            payroll.generatePaySlips();
+            break;
+        case 6: // View Employee details
+            // TODO: Implement getEmployee(employeeID) function and put it here
+            break;
+        default:
+            cout << "Invalid number";
+        }
+    } while (response != 1);
+
+    return 0;
 }
