6aff0a45e1c8286703d3617407550321028885eb
Safwan <safwan.bin.noor@gmail.com>
2024-10-14 16:36:48 +1030

    Initialised totalPayCost in constructor

--- Payroll.cpp
+++ Payroll.cpp
@@ -2,57 +2,53 @@
 
 using namespace std;
 
-Payroll::Payroll() : Payroll(0) {}
+Payroll::Payroll() {
+  this->companyFund = 0;
+  this->totalPayRollCost = 0;
+}
 
 // Constructor that initialises company fund
-Payroll::Payroll(double companyFund) { this->companyFund = companyFund; }
+Payroll::Payroll(double companyFund) {
+  this->companyFund = companyFund;
+  this->totalPayRollCost = 0;
+}
 
-// Pays the outstanding PayRollCost and returns true if funds are sufficient. Returns false if not
-bool Payroll::processPayRoll()
-{
-  if (companyFund < totalPayRollCost)
-  {
+// Pays the outstanding PayRollCost and returns true if funds are sufficient.
+// Returns false if not
+bool Payroll::processPayRoll() {
+  if (companyFund < totalPayRollCost) {
     return false;
-  }
-  else
+  } else
     companyFund -= totalPayRollCost;
   totalPayRollCost = 0;
   return true;
 }
 
 // Calculates the total money needed from the company fund
-void Payroll::calculateTotalCost()
-{
+void Payroll::calculateTotalCost() {
   // Reset the cost to 0
   totalPayRollCost = 0;
 
-  // Go through the employees and calculate their total cost using an iterator to iterate over the vector
-  for (auto it = employees.begin(); it != employees.end(); ++it)
-  {
+  // Go through the employees and calculate their total cost using an iterator
+  // to iterate over the vector
+  for (auto it = employees.begin(); it != employees.end(); ++it) {
     totalPayRollCost += it->calculateTotalPayment();
   }
 }
 
 // Add an Employee object to the vector
-void Payroll::addEmployee(Employee employee)
-{
-  employees.push_back(employee);
-}
+void Payroll::addEmployee(Employee employee) { employees.push_back(employee); }
 
-/* Takes an employeeID and removes the relevant employee from the payroll and returns true.
- Returns false if no such employee exists. */
-bool Payroll::removeEmployee(int employeeID)
-{
-  // Search through the array to find the index using an iterator to iterate over the vector
-  for (auto it = employees.begin(); it != employees.end();)
-  {
-    if (employeeID == it->getEmployeeID())
-    {
+/* Takes an employeeID and removes the relevant employee from the payroll and
+ returns true. Returns false if no such employee exists. */
+bool Payroll::removeEmployee(int employeeID) {
+  // Search through the array to find the index using an iterator to iterate
+  // over the vector
+  for (auto it = employees.begin(); it != employees.end();) {
+    if (employeeID == it->getEmployeeID()) {
       employees.erase(it);
       return true;
-    }
-    else
-    {
+    } else {
       ++it;
     }
   }
@@ -60,31 +56,26 @@ bool Payroll::removeEmployee(int employeeID)
 }
 
 // Add payslip to the payslip array
-void Payroll::addPaySlip(PaySlip payslip)
-{
-  payslips.push_back(payslip);
-}
+void Payroll::addPaySlip(PaySlip payslip) { payslips.push_back(payslip); }
 
-// Returns employee details as a list of strings of the form id|name|position|age
-std::string *Payroll::getEmployeeDetails()
-{
+// Returns employee details as a list of strings of the form
+// id|name|position|age
+std::string *Payroll::getEmployeeDetails() {
   std::string *details = new std::string[employees.size()];
-  for (int i = 0; i < (int)employees.size(); i++)
-  {
+  for (int i = 0; i < (int)employees.size(); i++) {
     Employee employee = employees[i];
     details[i] = to_string(employee.getEmployeeID()) + " | " +
-                 employee.getName() + " | " +
-                 employee.getPosition() + " | " +
+                 employee.getName() + " | " + employee.getPosition() + " | " +
                  to_string(employee.getAge());
   }
   return details;
 }
 
 // Generate payslips of all employees and add to outstanding cost
-void Payroll::generatePaySlips()
-{
-  for (PaySlip payslip : payslips)
-  { // Iterates over all Payslips in the "payslip" vector and stores in variable payslip
+void Payroll::generatePaySlips() {
+  for (PaySlip payslip :
+       payslips) {  // Iterates over all Payslips in the "payslip" vector and
+                    // stores in variable payslip
     payslip.generateSlip();
   }
   calculateTotalCost();
